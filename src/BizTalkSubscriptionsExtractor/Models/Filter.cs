6b83e8531b6d347e6e550e0a29ef935bf1cc3076
Wael Alissa <wael.alissa@outlook.com>
2023-05-23 11:47:49 +0200

    Upload files

---
+++ src/BizTalkSubscriptionsExtractor/Models/Filter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BizTalkSubscriptionsExtractor.Models
+{
+    public class Filter
+    {
+        [XmlIgnore]
+        public Guid FilterGroupId { get; set; }
+        [XmlIgnore]
+        public long FilterId {get;set;}
+        [XmlAttribute]
+        public string Property { get; set; }
+        [XmlAttribute] 
+        public string Operator { get; set; }
+        [XmlText()]
+        public string Value { get; set; }
+    }
+}
